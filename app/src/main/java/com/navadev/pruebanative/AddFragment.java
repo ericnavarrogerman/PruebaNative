fc3b151910b14d0dd515eb314cb2c4bf3da1fa67
eric jose navarro german <eric.navarro@novatec.com.co>
2023-03-07 06:38:04 -0500

    rename template classes

--- app/src/main/java/com/navadev/pruebanative/AddFragment.java
+++ app/src/main/java/com/navadev/pruebanative/AddFragment.java
@@ -7,13 +7,12 @@ import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.navigation.fragment.NavHostFragment;
 
-import com.navadev.pruebanative.databinding.FragmentFirstBinding;
+import com.navadev.pruebanative.databinding.FragmentAddBinding;
 
-public class FirstFragment extends Fragment {
+public class AddFragment extends Fragment {
 
-    private FragmentFirstBinding binding;
+    private FragmentAddBinding binding;
 
     @Override
     public View onCreateView(
@@ -21,7 +20,7 @@ public class FirstFragment extends Fragment {
             Bundle savedInstanceState
     ) {
 
-        binding = FragmentFirstBinding.inflate(inflater, container, false);
+        binding = FragmentAddBinding.inflate(inflater, container, false);
         return binding.getRoot();
 
     }
@@ -29,13 +28,7 @@ public class FirstFragment extends Fragment {
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        binding.buttonFirst.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                NavHostFragment.findNavController(FirstFragment.this)
-                        .navigate(R.id.action_FirstFragment_to_SecondFragment);
-            }
-        });
+
     }
 
     @Override
--- app/src/main/java/com/navadev/pruebanative/ListFragment.java
+++ app/src/main/java/com/navadev/pruebanative/ListFragment.java
@@ -9,11 +9,12 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 
-import com.navadev.pruebanative.databinding.FragmentSecondBinding;
+import com.navadev.pruebanative.databinding.FragmentListBinding;
 
-public class SecondFragment extends Fragment {
 
-    private FragmentSecondBinding binding;
+public class ListFragment extends Fragment {
+
+    private FragmentListBinding binding;
 
     @Override
     public View onCreateView(
@@ -21,7 +22,7 @@ public class SecondFragment extends Fragment {
             Bundle savedInstanceState
     ) {
 
-        binding = FragmentSecondBinding.inflate(inflater, container, false);
+        binding = FragmentListBinding.inflate(inflater, container, false);
         return binding.getRoot();
 
     }
@@ -29,13 +30,8 @@ public class SecondFragment extends Fragment {
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        binding.buttonSecond.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                NavHostFragment.findNavController(SecondFragment.this)
-                        .navigate(R.id.action_SecondFragment_to_FirstFragment);
-            }
-        });
+        binding.buttonAdd.setOnClickListener(view1 -> NavHostFragment.findNavController(ListFragment.this)
+                .navigate(R.id.action_ListFragment_to_AddFragment));
     }
 
     @Override
